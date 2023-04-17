3f8770803866b72b731a6a73b035f6eb850d67f0
Dino <dinocraftdidi@gmail.com>
2023-04-17 08:06:00 +0200

    todo

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <div>Bonjour {{ token.user.firstname }} {{ token.user.lastname }} !</div>
+
 </template>
 
 <script setup>
-
-const headers = useRequestHeaders(['cookie']);
-const { data: token } = await useFetch('/api/token', { headers })
-
-
 definePageMeta({
   middleware: 'auth',
   layout: 'admin'
---
+++ pages/admin/pages.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dark:text-white">
+      Test ijhgbfdjlhb ^fdihnbdmloklùkjhd lkmlvijfdljkvn,
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  middleware: 'auth',
+  layout: 'admin'
+})
+</script>
+
+<style scoped>
+
+</style>
