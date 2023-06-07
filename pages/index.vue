c3e6dbd4e09fc16b12b43d9a708fa39853b1c11b
Mael <dinocraftdidi@gmail.com>
2023-06-07 17:50:48 +0200

    feat: edit main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,27 @@
 <template>
-  <p>Message is: {{ message }}</p>
-  <input v-model="message" placeholder="edit me" />
-
-
+    <div class="p-4 space-y-4">
+      <div class="flex flex-col items-center">
+        <h1 class="text-3xl font-bold text-center dark:text-white">Bienvenue à notre élevage de Maine Coon!</h1>
+        <img class="mt-4" src="/path/to/image.jpg" alt="Maine Coon Cat">
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "index",
   data() {
     return {
-      message: ''
-    }
-  }
-}
+      cats: [
+        {
+          name: 'Fluffy',
+          image: '/path/to/image.jpg',
+          description: 'Fluffy est un chat magnifique avec une personnalité douce.',
+        },
+        // Plus de chats ici...
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
